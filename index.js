3d7b0b87511d968215166a5f581c273306d8a102
xuduo <xuduo@smzdm.com>
2017-07-25 16:10:53 +0800

    have some trouble

--- index.js
+++ index.js
@@ -2,25 +2,55 @@ var http = require('http');
 var fs = require('fs');
 var cheerio = require('cheerio');
 var request = require('request');
+var path = require('path');
 var url = ''
 
 //操作页面
 function filterChapters(html){
+  var title = './src/'+url.split('.net/')[1]
   var $ = cheerio.load(html)
   var chapters = $('.post-content')
   var arrTitle = chapters.find('.su-spoiler-title').text()
   var group = chapters.find('#con_tag').find('a').text()
   var images = chapters.find('.gallery-item').find('a')
-  images.each(function(item){
+  mkdir(title);
+  images.each(function(i,item){
     var img_url = $(this).attr('href')
     var img_title = $(this).attr('title')+'.jpg'
+    console.log('正在下载第'+ i +'张')
     request.head(img_url,function(err,res,body){
         if(err){
-            console.log(err);
+          console.log('失败');
+        }else{
+          request(img_url).pipe(fs.createWriteStream(title + img_title))
+          console.log('成功')
         }
     });
-    request(img_url).pipe(fs.createWriteStream('./src/' + img_title))
+
   })
+}
+//使用时第二个参数可以忽略
+function mkdir(dirpath,dirname){
+  //判断是否是第一次调用
+  if(typeof dirname === "undefined"){
+    if(fs.existsSync(dirpath)){
+      return;
+    }else{
+      mkdir(dirpath,path.dirname(dirpath));
+    }
+  }else{
+      //判断第二个参数是否正常，避免调用时传入错误参数
+    if(dirname !== path.dirname(dirpath)){
+      mkdir(dirpath);
+      return;
+    }
+    if(fs.existsSync(dirname)){
+      fs.mkdirSync(dirpath)
+    }else{
+      mkdir(dirname,path.dirname(dirname));
+      fs.mkdirSync(dirpath);
+    }
+  }
 }
 //获取页面信息
 http.get(url,function(res){
--- pages.js
+++ pages.js
@@ -27,7 +27,7 @@ function filter(html){
     getImg(url)
   })
 }
-function getImg(url){
+function getImg(urlItem){
   http.get(url,function(res){
     var html = ''
 
@@ -42,21 +42,48 @@ function getImg(url){
     console.log('获取数据有误！')
   })
   var filterChapters = function(html){
+    var title = './src/'+url.split('.net/')[1];
     var $ = cheerio.load(html)
     var chapters = $('.post-content')
     var arrTitle = chapters.find('.su-spoiler-title').text()
     var group = chapters.find('#con_tag').find('a').text()
     var images = chapters.find('.gallery-item').find('a')
-    images.each(function(item){
+    mkdir(title)
+    images.each(function(i,item){
+      console.log('正在下载第'+ i +'张')
       var img_url = $(this).attr('href')
       var img_title = $(this).attr('title')+'.jpg'
       request.head(img_url,function(err,res,body){
           if(err){
-              console.log(err);
+            console.log('失败');
+          }else{
+            request(img_url).pipe(fs.createWriteStream(title + img_title))
+            console.log('成功')
           }
       });
-      request(img_url).pipe(fs.createWriteStream('./src/' + img_title))
     })
-
+  }
+  //使用时第二个参数可以忽略
+  var mkdir = function(dirpath,dirname){
+    //判断是否是第一次调用
+    if(typeof dirname === "undefined"){
+      if(fs.existsSync(dirpath)){
+        return;
+      }else{
+        mkdir(dirpath,path.dirname(dirpath));
+      }
+    }else{
+        //判断第二个参数是否正常，避免调用时传入错误参数
+      if(dirname !== path.dirname(dirpath)){
+        mkdir(dirpath);
+        return;
+      }
+      if(fs.existsSync(dirname)){
+        fs.mkdirSync(dirpath)
+      }else{
+        mkdir(dirname,path.dirname(dirname));
+        fs.mkdirSync(dirpath);
+      }
+    }
   }
 }
